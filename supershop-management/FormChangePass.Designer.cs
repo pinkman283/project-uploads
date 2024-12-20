27e910b6f62af7d6f1127809d682b3fb2d7792d5
Shihab Shovon <122913165+pinkman283@users.noreply.github.com>
2024-12-21 05:34:01 +0600

    Add files via upload

---
+++ supershop-management/FormChangePass.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SuperShop_Management
+{
+    partial class FormChangePass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNp1 = new System.Windows.Forms.TextBox();
+            this.txtNp2 = new System.Windows.Forms.TextBox();
+            this.btnclose2 = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightSlateGray;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(126, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 30);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "New Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightSlateGray;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(276, 30);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Confirm New Password";
+            // 
+            // txtNp1
+            // 
+            this.txtNp1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtNp1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtNp1.Font = new System.Drawing.Font("Cascadia Code ExtraLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNp1.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtNp1.Location = new System.Drawing.Point(340, 108);
+            this.txtNp1.Name = "txtNp1";
+            this.txtNp1.Size = new System.Drawing.Size(338, 35);
+            this.txtNp1.TabIndex = 8;
+            this.txtNp1.Text = "Password";
+            this.txtNp1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNp2
+            // 
+            this.txtNp2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtNp2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtNp2.Font = new System.Drawing.Font("Cascadia Code ExtraLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNp2.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtNp2.Location = new System.Drawing.Point(340, 164);
+            this.txtNp2.Name = "txtNp2";
+            this.txtNp2.Size = new System.Drawing.Size(338, 35);
+            this.txtNp2.TabIndex = 9;
+            this.txtNp2.Text = "Confrim Password";
+            this.txtNp2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtNp2.Enter += new System.EventHandler(this.txtNp2_Enter);
+            // 
+            // btnclose2
+            // 
+            this.btnclose2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnclose2.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclose2.Location = new System.Drawing.Point(194, 219);
+            this.btnclose2.Name = "btnclose2";
+            this.btnclose2.Size = new System.Drawing.Size(110, 87);
+            this.btnclose2.TabIndex = 19;
+            this.btnclose2.Text = "Close";
+            this.btnclose2.UseVisualStyleBackColor = false;
+            this.btnclose2.Click += new System.EventHandler(this.btnclose2_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnsave.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.Location = new System.Drawing.Point(460, 219);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(110, 87);
+            this.btnsave.TabIndex = 20;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = false;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // FormChangePass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 318);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnclose2);
+            this.Controls.Add(this.txtNp2);
+            this.Controls.Add(this.txtNp1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormChangePass";
+            this.Text = "FormChangePass";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNp1;
+        private System.Windows.Forms.TextBox txtNp2;
+        private System.Windows.Forms.Button btnclose2;
+        private System.Windows.Forms.Button btnsave;
+    }
+}
---
+++ supershop-management/FormSelectProduct.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace SuperShop_Management
+{
+    partial class FormSelectProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSelectProduct));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblConfirmation = new System.Windows.Forms.Label();
+            this.btnCon = new System.Windows.Forms.Button();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dgvShowProduct = new System.Windows.Forms.DataGridView();
+            this.ProductId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShowProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.lblConfirmation);
+            this.panel1.Controls.Add(this.btnCon);
+            this.panel1.Controls.Add(this.btnConfirm);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(855, 189);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblConfirmation
+            // 
+            this.lblConfirmation.AutoSize = true;
+            this.lblConfirmation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblConfirmation.Font = new System.Drawing.Font("Arial Narrow", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirmation.Location = new System.Drawing.Point(539, 78);
+            this.lblConfirmation.Name = "lblConfirmation";
+            this.lblConfirmation.Size = new System.Drawing.Size(328, 40);
+            this.lblConfirmation.TabIndex = 24;
+            this.lblConfirmation.Text = "Once You Click Confrim You can\'t Cancel Transaction!\r\nGo To Cart to see Selected " +
+    "Product";
+            this.lblConfirmation.Visible = false;
+            // 
+            // btnCon
+            // 
+            this.btnCon.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnCon.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCon.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCon.Location = new System.Drawing.Point(602, 117);
+            this.btnCon.Name = "btnCon";
+            this.btnCon.Size = new System.Drawing.Size(166, 72);
+            this.btnCon.TabIndex = 23;
+            this.btnCon.Text = "Confirm";
+            this.btnCon.UseVisualStyleBackColor = false;
+            this.btnCon.Click += new System.EventHandler(this.btnCon_Click);
+            this.btnCon.MouseEnter += new System.EventHandler(this.btnCon_MouseEnter);
+            this.btnCon.MouseLeave += new System.EventHandler(this.btnCon_MouseLeave);
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnConfirm.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirm.Image = ((System.Drawing.Image)(resources.GetObject("btnConfirm.Image")));
+            this.btnConfirm.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConfirm.Location = new System.Drawing.Point(58, 117);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(183, 72);
+            this.btnConfirm.TabIndex = 21;
+            this.btnConfirm.Text = "Go To Cart";
+            this.btnConfirm.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnConfirm.UseVisualStyleBackColor = false;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnAdd.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnAdd.Image")));
+            this.btnAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdd.Location = new System.Drawing.Point(335, 117);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(177, 72);
+            this.btnAdd.TabIndex = 20;
+            this.btnAdd.Text = "Add To Cart";
+            this.btnAdd.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(208, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(393, 44);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Select Product and Click Add";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(365, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 277);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(855, 348);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dgvShowProduct);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(855, 401);
+            this.panel2.TabIndex = 3;
+            // 
+            // dgvShowProduct
+            // 
+            this.dgvShowProduct.AllowUserToAddRows = false;
+            this.dgvShowProduct.AllowUserToDeleteRows = false;
+            this.dgvShowProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvShowProduct.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ProductId,
+            this.ProductName,
+            this.Price,
+            this.Quantity});
+            this.dgvShowProduct.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvShowProduct.Location = new System.Drawing.Point(0, 0);
+            this.dgvShowProduct.Name = "dgvShowProduct";
+            this.dgvShowProduct.ReadOnly = true;
+            this.dgvShowProduct.RowHeadersWidth = 62;
+            this.dgvShowProduct.RowTemplate.Height = 28;
+            this.dgvShowProduct.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvShowProduct.Size = new System.Drawing.Size(855, 401);
+            this.dgvShowProduct.TabIndex = 3;
+            // 
+            // ProductId
+            // 
+            this.ProductId.DataPropertyName = "ProductId";
+            this.ProductId.HeaderText = "Product Id";
+            this.ProductId.MinimumWidth = 8;
+            this.ProductId.Name = "ProductId";
+            this.ProductId.ReadOnly = true;
+            this.ProductId.Width = 120;
+            // 
+            // ProductName
+            // 
+            this.ProductName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ProductName.DataPropertyName = "ProductName";
+            this.ProductName.HeaderText = "Product Name";
+            this.ProductName.MinimumWidth = 8;
+            this.ProductName.Name = "ProductName";
+            this.ProductName.ReadOnly = true;
+            // 
+            // Price
+            // 
+            this.Price.DataPropertyName = "Price";
+            this.Price.HeaderText = "Price";
+            this.Price.MinimumWidth = 8;
+            this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
+            this.Price.Width = 150;
+            // 
+            // Quantity
+            // 
+            this.Quantity.DataPropertyName = "Quantity";
+            this.Quantity.HeaderText = "Quantity";
+            this.Quantity.MinimumWidth = 8;
+            this.Quantity.Name = "Quantity";
+            this.Quantity.ReadOnly = true;
+            this.Quantity.Width = 90;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnBack.Location = new System.Drawing.Point(3, 195);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(136, 79);
+            this.btnBack.TabIndex = 4;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(272, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(250, 44);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Available Product";
+            // 
+            // btnShow
+            // 
+            this.btnShow.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnShow.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnShow.Location = new System.Drawing.Point(704, 205);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(151, 72);
+            this.btnShow.TabIndex = 22;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = false;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // FormSelectProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 625);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormSelectProduct";
+            this.Text = "FormSelectProduct";
+            this.Load += new System.EventHandler(this.FormSelectProduct_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShowProduct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dgvShowProduct;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnCon;
+        private System.Windows.Forms.Label lblConfirmation;
+    }
+}
